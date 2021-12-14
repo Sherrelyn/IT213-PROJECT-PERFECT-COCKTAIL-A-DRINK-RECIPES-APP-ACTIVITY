2f2b45aa2dfed2711cbdc49deed9be629b5f4be4
Sherrelyn <chavezsherrelyn13@gmail.com>
2021-12-13 21:33:03 -0800

    Added a method that dislays the single recipe. Added variables and elements. Added a method that displays a custom messsage. Added html and an htmlelement. Added functionality to set a timeout. Added a method that clear the previous results. All changes added to ui.js.

--- js/UI.js
+++ js/UI.js
@@ -119,4 +119,47 @@ class UI {
 
         return ingredientsTemplate;
     }
+          // Display single recipe
+    displaySingleRecipe(recipe){
+        // Get variables
+        const modalTitle = document.querySelector('.modal-title'),
+              modalDescription = document.querySelector('.modal-body .description-text'),
+              modalIngredients = document.querySelector('.modal-body .ingredient-list .list-group');
+
+        // Set the values
+        modalTitle.innerHTML = recipe.strDrink;
+        modalDescription.innerHTML = recipe.strInstructions;
+
+        // Display the ingredients
+        modalIngredients.innerHTML = this.displayIngredients(recipe);
+    }
+
+    // Displays a custom message
+    printMessage(message, className) {
+        const div = document.createElement('div');
+
+        // Add the HTMl
+        div.innerHTML = `
+            <div class="alert alert-dismissible alert-${className}">
+                <button type="button" class="close" data-dismiss="alert"></button>
+                ${message}
+            </div>
+        `;
+
+        // Insert before
+        const reference = document.querySelector('.jumbotron h1');
+        const parentNode = reference.parentElement;
+        parentNode.insertBefore(div, reference);
+
+        // Remove after 3 seconds
+        setTimeout(() => {
+            document.querySelector('.alert').remove();
+        }, 3000);
+    }
+
+    // Clear previous results
+    clearResults() {
+        const resultsDiv = document.querySelector('#results');
+        resultsDiv.innerHTML = '';
+    }
 }
